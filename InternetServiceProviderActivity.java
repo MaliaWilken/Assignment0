d894d7d7579a619266dd4bc440c87c3d7202777b
Malia Wilken <mawilken@cpp.edu>
2025-02-04 23:43:24 -0800

    Adjusted code

--- InternetServiceProviderActivity.java
+++ InternetServiceProviderActivity.java
@@ -8,24 +8,24 @@ public class InternetServiceProviderActivity {
 		System.out.println("Enter Number of Hours Used: ");
 		
 		Scanner scnr = new Scanner(System.in);
-		String choice = scnr.nextLine().toUpperCase();
+		String choice = scnr.nextLine();//.toUpperCase();
 		int hours = scnr.nextInt();
 		int paidHours;
 		double monthlyBill = 0.0;
 		
 		switch (choice) {
 		case "A":
-		//case "a":
+		case "a":
 			paidHours = Math.max(0,  hours - 10);
 			monthlyBill = 9.95 + 2*(paidHours);
 			break;
 		case "B":
-		//case "b":
+		case "b":
 			paidHours = Math.max(0,  hours - 20);
 			monthlyBill = 13.95 + 1*(paidHours);
 			break;
 		case "C":
-		//case "c":
+		case "c":
 			monthlyBill = 19.95;
 			break;
 		default:
